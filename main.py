06bb645aeb334f5537ff242439ee464d08aa1041
Ikwuka Okoye <ikwukao@proton.me>
2026-05-20 08:04:53 -0400

    Add new method

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ class MusicalInstrument:
         self.name = name
         self.instrument_type = instrument_type
 
+    def play(self):
+        print(f'The {self.name} is fun to play!')
+
 
 instrument_1 = MusicalInstrument("Oboe", "woodwind")
 instrument_2 = MusicalInstrument("Trumpet", "brass")
 
-
-print(instrument_1.name)
-print(instrument_1.instrument_type)
-
-print(instrument_2.name)
-print(instrument_2.instrument_type)
