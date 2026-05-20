bf8ad897d662bf890a51c4772aef1edcd3def1df
Ikwuka Okoye <ikwukao@proton.me>
2026-05-20 07:51:51 -0400

    Update codebase and call class

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 class MusicalInstrument:
-    pass
+    def __init__(self, name, instrument_type):
+        self.name = name
+        self.instrument_type = instrument_type
+
+
+instrument_1 = MusicalInstrument("Oboe", "woodwind")
+instrument_2 = MusicalInstrument("Trumpet", "brass")
